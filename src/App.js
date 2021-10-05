981d1e226f14f2429ca2e5427c961798526c63b0
Uliana <ulyana.yuzko@gmail.com>
2021-10-05 14:04:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './free-sticker-love-4383913.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Натренуй мене, дЭтка!
         </p>
         <a
           className="App-link"
